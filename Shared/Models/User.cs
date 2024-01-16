b478ee77f8f48856ab391adc808b07acdb21a299
kruddick <kruddick@gmail.com>
2024-01-16 06:05:53 -0700

    Adding auth request and response.

---
+++ Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.Models;
+public class User
+{
+	public string Id { get; set; } = Guid.NewGuid().ToString();
+	public string Name { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+	public string Password { get; set; } = string.Empty;
+	public string Role { get; set; } = string.Empty;
+	public string Token { get; set; } = string.Empty;
+	public string RefreshToken { get; set; } = string.Empty;
+	public DateTime RefreshTokenExpiryTime { get; set; } = DateTime.UtcNow.AddDays(7);
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Shared/Models/UserAuthRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Shared.Models;
+public class UserAuthRequest
+{
+}
